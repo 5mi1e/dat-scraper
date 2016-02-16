781b229014e9d0b55f25ab70fd605b57da05effb
Smile <5mi1e@users.noreply.github.com>
2016-02-16 17:07:43 +0200

    Date & time for tvnet.lv comments #fix2 Working TODO rus.tvnet

--- comment_scraper/spiders/tvnet.py
+++ comment_scraper/spiders/tvnet.py
@@ -43,14 +43,18 @@ class TvnetSpider(scrapy.Spider):
         if response.xpath(
                 '//ol[@class="commentary"]/li/div[@class="comment-container"]/div/div[@class="comment-tool-container"]//@data-comment-id').extract():
             for comment in response.xpath('//ol[@class="commentary"]/li'):
-                try:
+                #try:
+                if comment.xpath('div[@class="comment-container"]'):
                     item = CommentScraperItem()
                     article_id = re.search('(?<=\/)([0-9]*)(?=\-)', response.url).group()
                     item['article_id'] = article_id
-                    date_time = self.string_to_datetime(''.join(comment.xpath('div[@class="comment-container"]/div/span[@class="date"]/a/text()').extract()))
+                    if 'http://rus.' in response.url:
+                        item['comment_date'] = date
+                    else:
+                        date_time = self.string_to_datetime(''.join(comment.xpath('div[@class="comment-container"]/div/span[@class="date"]/a/text()').extract()))
+                        item['comment_date'] = date_time.strftime('%Y-%m-%d')
+                        item['comment_time'] = date_time.strftime('%H:%M:%S')
 
-                    item['comment_date'] = date_time.strftime('%Y-%m-%d')
-                    item['comment_time'] = date_time.strftime('%H:%M:%S')
                     item['comment_id'] = comment.xpath('div[@class="comment-container"]/div/div[@class="comment-tool-container"]//@data-comment-id').extract()[0]
                     comment_author = comment.css('span.author::text').extract()
                     if comment_author:
@@ -83,8 +87,8 @@ class TvnetSpider(scrapy.Spider):
                             'div[@class="comment-container"]/div/span[@class="author"]/img[@class="top-commentator"]'):
                         item['top100'] = 1
                     yield item
-                except:
-                    pass
+                #except:
+                #    pass
             next_page = response.xpath('//a[contains(@class, "next")]/@href').extract()
             if next_page and next_page[0] != "#":
                 yield scrapy.Request(next_page[0], callback=lambda r, date=date: self.parse_comments(r, date))
@@ -117,6 +121,6 @@ class TvnetSpider(scrapy.Spider):
                 time = re.search( '\d.:\d.',dstring).group()
                 m = {u'janvārī': 1, u'februārī': 2, u'martā': 3, u'aprīlī':4, u'maijā':5, u'jūnijā':6, u'jūlijā':7, u'augustā':8, u'septembrī':9, u'octobrī':10, u'novembrī':11, u'decembrī':12}
                 month_num = m[month_string]
-                date = str(year)+'-'+str(month_num)+'-'+str(day)
-                commentDT = datetime.datetime.strptime(date+' '+time, '%Y-%m-%d %H:%M')
+                date_s = str(year)+'-'+str(month_num)+'-'+str(day)
+                commentDT = datetime.datetime.strptime(date_s+' '+time, '%Y-%m-%d %H:%M')
         return commentDT
