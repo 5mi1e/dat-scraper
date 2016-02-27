9105a695209601ac675dfa8cf5fc9d948b145622
Smile <5mi1e@users.noreply.github.com>
2016-02-27 19:44:29 +0200

    apollo, check if comments exist

--- comment_scraper/spiders/apollo.py
+++ comment_scraper/spiders/apollo.py
@@ -45,36 +45,37 @@ class ApolloSpider(scrapy.Spider):
 
     def parse_comments(self, response):
         for comment in response.xpath('//div[@class="article-comments"]/div[ contains(@class, "article-comment")]'):
-            item = CommentScraperItem()
-            item['article_id'] = re.search('(?<=\/)\d{4,10}', response.url).group() # TODO: Test this
-            item['comment_id'] = re.search('([0-9]+)', comment.xpath('@id').extract()[0]).group()
-            auth = comment.xpath(
-                'div/span[@class="article-comment-pic"]//span[contains(@class, "icon-soc")]/@title').extract()
-            if auth:
-                item['author_auth'] = auth[0]
-            item['comment_author'] = ''.join(
-                comment.xpath('div/span[@class="article-comment-pic"]/img/@title').extract())
-            date = dateutil.parser.parse(''.join(comment.xpath('div/time/@datetime').extract()))
-            item['comment_date'] = datetime.datetime.strftime(date, '%Y-%m-%d')
-            item['comment_time'] = datetime.datetime.strftime(date, '%H:%M:%S')
-            pos = comment.xpath('div/div/div/div/a[ contains(@class, "plus") ]/text()').extract()
-            if pos:
-                item['recommend_pos'] = int(pos[0])
-            neg = comment.xpath('div/div/div/div/a[ contains(@class, "minus") ]/text()').extract()
-            if neg:
-                item['recommend_neg'] = int(neg[0])
+            if not comment.xpath('p[@class="no-comments-alert"]'):
+                item = CommentScraperItem()
+                item['article_id'] = re.search('(?<=\/)\d{4,10}', response.url).group() # TODO: Test this
+                item['comment_id'] = re.search('([0-9]+)', comment.xpath('@id').extract()[0]).group()
+                auth = comment.xpath(
+                    'div/span[@class="article-comment-pic"]//span[contains(@class, "icon-soc")]/@title').extract()
+                if auth:
+                    item['author_auth'] = auth[0]
+                item['comment_author'] = ''.join(
+                    comment.xpath('div/span[@class="article-comment-pic"]/img/@title').extract())
+                date = dateutil.parser.parse(''.join(comment.xpath('div/time/@datetime').extract()))
+                item['comment_date'] = datetime.datetime.strftime(date, '%Y-%m-%d')
+                item['comment_time'] = datetime.datetime.strftime(date, '%H:%M:%S')
+                pos = comment.xpath('div/div/div/div/a[ contains(@class, "plus") ]/text()').extract()
+                if pos:
+                    item['recommend_pos'] = int(pos[0])
+                neg = comment.xpath('div/div/div/div/a[ contains(@class, "minus") ]/text()').extract()
+                if neg:
+                    item['recommend_neg'] = int(neg[0])
 
-            item['comment_url'] = response.url
-            comment_txt = comment.xpath(
-                'div[@class="article-comment-content"]/text() | div[@class="article-comment-content"]/a/@href'
-                ).extract()
-            comment_txt = ' '.join(comment_txt)
-            if "/comment/external" in comment_txt:
-                comment_txt = re.sub('(/comment/external.url=)', "", comment_txt)
-                comment_txt = re.sub('(&hash=[a-z0-9]*)', "", comment_txt)
-                comment_txt = urllib.unquote(comment_txt)
-            item['comment_txt'] = comment_txt
-            yield item
+                item['comment_url'] = response.url
+                comment_txt = comment.xpath(
+                    'div[@class="article-comment-content"]/text() | div[@class="article-comment-content"]/a/@href'
+                    ).extract()
+                comment_txt = ' '.join(comment_txt)
+                if "/comment/external" in comment_txt:
+                    comment_txt = re.sub('(/comment/external.url=)', "", comment_txt)
+                    comment_txt = re.sub('(&hash=[a-z0-9]*)', "", comment_txt)
+                    comment_txt = urllib.unquote(comment_txt)
+                item['comment_txt'] = comment_txt
+                yield item
         next_page = response.xpath('//a[@class="pageing-button-next"]/@href').extract()
         if next_page:
             yield scrapy.Request('http://apollo.tvnet.lv' + ''.join(next_page), callback=self.parse_comments)
