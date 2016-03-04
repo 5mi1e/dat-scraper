2492486c3574809513131e195bb6b021bc7da519
Smile <5mi1e@users.noreply.github.com>
2016-03-04 18:34:21 +0200

    delfi v1

--- comment_scraper/spiders/delfi.py
+++ comment_scraper/spiders/delfi.py
@@ -6,9 +6,10 @@ import json
 from scrapy.selector import Selector
 from comment_scraper.items import CommentScraperItem
 
+
 class DelfiSpider(scrapy.Spider):
     name = "delfi"
-    script="""
+    script = """
         function main(splash)
             splash.images_enabled = false
             assert(splash:autoload("https://code.jquery.com/jquery-2.1.3.min.js"))
@@ -41,6 +42,7 @@ class DelfiSpider(scrapy.Spider):
 
         end
     """
+
     def __init__(self, dfrom=None, dto=None, *args, **kwargs):
         super(DelfiSpider, self).__init__(*args, **kwargs)
         if not dfrom or not dto:
@@ -50,10 +52,10 @@ class DelfiSpider(scrapy.Spider):
             self.start_date = datetime.datetime.strptime(dfrom, '%Y-%m-%d').date()
             self.end_date = datetime.datetime.strptime(dto, '%Y-%m-%d').date()
         self.step = datetime.timedelta(days=1)
-        self.allowed_domains = ['delfi.lv']
+        self.allowed_domains = ['delfi.lv']  # TODO: rus.delfi
         self.start_urls = (
             'http://www.delfi.lv/archive/?tod={2}-{1}-{0}&fromd={2}-{1}-{0}&channel=0&category=0&query=#'.format(
-                str(self.start_date.year), str(self.start_date.month),str(self.start_date.day)),
+                str(self.start_date.year), str(self.start_date.month), str(self.start_date.day)),
         )
         self.logger.info('\033[92m' + 'Datumu diapazons: %s - %s' + '\033[0m', self.start_date, self.end_date)
 
@@ -63,10 +65,10 @@ class DelfiSpider(scrapy.Spider):
             if comment_url:
                 comment_url_unreg = re.sub('reg=1', 'reg=0', comment_url[0])
                 for url in comment_url[0], comment_url_unreg:
-                    yield scrapy.Request(url, callback=self.parse_comments,meta={
-                        'splash':{
-                            'args':{'lua_source': self.script,},
-                            'endpoint':'execute',
+                    yield scrapy.Request(url, callback=self.parse_comments, meta={
+                        'splash': {
+                            'args': {'lua_source': self.script,},
+                            'endpoint': 'execute',
                         }
                     })
         next_page = response.xpath('//a[@class="item next"]/@href').extract()
@@ -74,48 +76,98 @@ class DelfiSpider(scrapy.Spider):
             next_page = next_page[0].strip('\n\t')
             next_page = re.sub('\n\t+', '', next_page)
             yield scrapy.Request('http://www.delfi.lv' + next_page, callback=self.parse)
-
+        else:
+            self.start_date += self.step
+            if self.start_date <= self.end_date:
+                url = 'http://www.delfi.lv/archive/?tod={2}-{1}-{0}&fromd={2}-{1}-{0}&channel=0&category=0&query=#'.format(
+                str(self.start_date.year), str(self.start_date.month), str(self.start_date.day))
+                yield scrapy.Request(url, callback=self.parse)
 
     def parse_comments(self, response):
         data = json.loads(response.body_as_unicode())
         url = data['url']
         sel = Selector(text=data['html'])
-
-        #from scrapy.shell import inspect_response
-        #inspect_response(response, self)
-
         comment_count = sel.xpath('//div[@id="comments-list"]/@data-count').extract()[0]
-        reg  = sel.xpath('//div[contains( @class, "thread-switcher-selected-reg")]')
+        reg = sel.xpath('//div[contains( @class, "thread-switcher-selected-reg")]')
         anon = sel.xpath('//div[contains( @class, "thread-switcher-selected-anon")]')
-
 
         if int(comment_count) > 0:
 
-            item = CommentScraperItem()
             if anon and "reg=0" in url:
-                for comment in sel.xpath('//div[@id="comments-list"]//div[contains(@class, "comment-post")]'):
-                    if not comment.xpath('div[@class="comment-author"]/a') and not comment.re('comment-deleted'):
-                        try:
-                            comment_author = comment.xpath('div[@class="comment-author"]/text()').extract()[0].strip('\n\t')
-                            item['comment_author'] = comment_author
-                        except:
-                            self.logger.info(url)
+                for comment in sel.xpath(
+                        '//div[@id="comments-list"]//div[contains(@class, "comment-post") and not(contains(@class, "comment-deleted")) and not(contains(@class, "comment-avatar-registered"))]'):
+                    item = CommentScraperItem()
+                    comment_author = comment.xpath('div[@class="comment-author"]/text()').extract()[0].strip('\n\t')
+                    comment_id = re.sub('[a-z]', '',
+                                        comment.xpath('a[@class="comment-list-comment-anchor"]/@name').extract()[0])
+                    date_time_str = comment.xpath('div[@class="comment-date"]/text()').extract()[0].strip('\n\t')
+                    date_time = datetime.datetime.strptime(date_time_str, '%d.%m.%Y %H:%M')
+                    comment_date = date_time.strftime('%Y-%m-%d')
+                    comment_time = date_time.strftime('%H:%M:%S')
+                    comment_txt = ''.join(comment.xpath('div[@class="comment-content"]/div[@class="comment-content-inner"]/text()').extract()).strip('\n\t')
+                    recommend_pos = ''.join(comment.xpath('div[@class="comment-votes"]/div[@class="comment-votes-up"]/a/text()').extract()).strip('\n\t')
+                    recommend_neg = ''.join(comment.xpath('div[@class="comment-votes"]/div[@class="comment-votes-down"]/a/text()').extract()).strip('\n\t')
+                    if not recommend_neg and not recommend_pos:
+                        recommend_neg = 0
+                        recommend_pos = 0
+                    article_id = re.search('(?<=id\=)(\d+)', url)
+                    if not article_id:
+                        article_id = re.search('(?<=\/)(\d+)', url).group()
+                    else:
+                        article_id = article_id.group()
+
+                    item['article_id'] = article_id
+                    item['comment_url'] = url
+                    item['comment_txt'] = comment_txt
+                    item['comment_date'] = comment_date
+                    item['comment_time'] = comment_time
+                    item['recommend_neg'] = int(recommend_neg)
+                    item['recommend_pos'] = int(recommend_pos)
+                    item['comment_author'] = comment_author
+                    item['comment_id'] = comment_id
+
+                    yield item
+
             elif reg and "reg=1" in url:
-                for comment in sel.xpath('//div[@id="comments-list"]//div[contains(@class, "comment-post")]'):
-                    if not comment.re('comment-deleted'):
-                        try:
-                            comment_author = comment.xpath('div[@class="comment-author"]/a/text()').extract()[0].strip('\n\t')
-                            item['comment_author'] = comment_author
-                        except:
-                            print comment.xpath('div[@class="comment-author"]/text()').extract()[0].strip('\n\t')
-                            self.logger.info(url)
+                for comment in sel.xpath(
+                        '//div[@id="comments-list"]//div[contains(@class, "comment-post") and not(contains(@class, "comment-deleted")) and not(contains(@class, "comment-avatar-anonymous"))]'):
+                    item = CommentScraperItem()
+                    comment_author = comment.xpath('div[@class="comment-author"]/a/text()').extract()[0].strip('\n\t')
+                    comment_id = re.sub('[a-z]', '',
+                                        comment.xpath('a[@class="comment-list-comment-anchor"]/@name').extract()[0])
+                    date_time_str = comment.xpath('div[@class="comment-date"]/text()').extract()[0].strip('\n\t')
+                    date_time = datetime.datetime.strptime(date_time_str, '%d.%m.%Y %H:%M')
+                    comment_date = date_time.strftime('%Y-%m-%d')
+                    comment_time = date_time.strftime('%H:%M:%S')
+                    comment_txt = ''.join(comment.xpath('div[@class="comment-content"]/div[@class="comment-content-inner"]/text()').extract()).strip('\n\t')
+                    recommend_pos = ''.join(comment.xpath('div[@class="comment-votes"]/div[@class="comment-votes-up"]/a/text()').extract()).strip('\n\t')
+                    recommend_neg = ''.join(comment.xpath('div[@class="comment-votes"]/div[@class="comment-votes-down"]/a/text()').extract()).strip('\n\t')
+                    if not recommend_neg and not recommend_pos:
+                        recommend_neg = 0
+                        recommend_pos = 0
+                    article_id = re.search('(?<=id\=)(\d+)', url)
+                    if not article_id:
+                        article_id = re.search('(?<=\/)(\d+)', url).group()
+                    else:
+                        article_id = article_id.group()
+
+                    item['article_id'] = article_id
+                    item['comment_url'] = url
+                    item['comment_txt'] = comment_txt
+                    item['comment_date'] = comment_date
+                    item['comment_time'] = comment_time
+                    item['recommend_neg'] = int(recommend_neg)
+                    item['recommend_pos'] = int(recommend_pos)
+                    item['comment_author'] = comment_author
+                    item['comment_id'] = comment_id
+
+                    yield item
 
             next_page = sel.xpath('//a[@class="comments-pager-arrow-last"]/@href').extract()
             if next_page:
-                yield scrapy.Request(next_page[0], callback=self.parse_comments,meta={
-                    'splash':{
-                        'args':{'lua_source': self.script,},
-                        'endpoint':'execute',
+                yield scrapy.Request(next_page[0], callback=self.parse_comments, meta={
+                    'splash': {
+                        'args': {'lua_source': self.script,},
+                        'endpoint': 'execute',
                     }
                 })
-            yield item
